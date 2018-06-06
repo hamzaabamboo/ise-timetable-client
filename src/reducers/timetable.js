d617476cf639c33b269e06b5a99d86ae22c55185
Tanyawat <hamzaabamboo@gmail.com>
2018-06-07 00:00:46 +0700

    Simple subject adding

--- src/reducers/timetable.js
+++ src/reducers/timetable.js
@@ -1,7 +1,16 @@
-import { SEARCH_RESULT, CLEAR_RESULT } from '../constants';
+import { SEARCH_RESULT, CLEAR_RESULT, ADD_SUBJECT } from '../constants';
+import moment from 'moment';
 
 const initialState = {
-	searchResult: []
+	searchResults: [],
+	createEvent: {
+		monday: [],
+		tuesday: [],
+		wednesday: [],
+		thursday: [],
+		friday: []
+	},
+	unannounced: []
 };
 
 export default (state = initialState, action) => {
@@ -15,6 +24,35 @@ export default (state = initialState, action) => {
 			return {
 				...state,
 				...action.payload
+			};
+		case ADD_SUBJECT:
+			let { times, ...rest } = action.payload;
+			let newTime = Object.assign({}, state.createEvent);
+			let newUnannounced = Array.from(state.unannounced);
+			let found = false;
+			times.forEach(e => {
+				if (e.day.toLowerCase() in state.createEvent) {
+					newTime[e.day.toLowerCase()].push({
+						...rest,
+						startTime: moment(e.startTime, 'hhmm'),
+						endTime: moment(e.endTime, 'hhmm')
+					});
+					found = true;
+				} else {
+					if (!found) {
+						newUnannounced.push({
+							id: action.payload.id,
+							name: action.payload.name,
+							section: action.payload.section
+						});
+						found = true;
+					}
+				}
+			});
+			return {
+				...state,
+				createEvent: newTime,
+				unannounced: newUnannounced
 			};
 		default:
 			return state;
